4ff68ed1e1513c947628e91fca3d13b9a0b5cb73
John Paeldin <john.paeldin@vesta.io>
2024-02-05 19:57:14 -0800

    ANALYTICS: Add vercel analytics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 import Head from "next/head";
+import Script from "next/script";
 import styles from "@/styles/Home.module.css";
 import Layout from "./component/layout/Layout";
 import { Inter } from "next/font/google";
-import Script from "next/script";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function Home() {
         <Script src="/js/custom.js" defer/>
         <main className={`${styles.main} ${inter.className}`}>
           <Layout className="main-container"/>
+          <Analytics />
         </main>
       <Script src="/js/bootstrap.bundle.min.js" />
     </>
